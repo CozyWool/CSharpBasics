5f632e18155e4ddbf2afc3592a977794b76e005c
CozyWool <vsergeev201530@gmail.com>
2025-10-20 07:46:17 +0500

    TextAnalysis fixes after review

--- TextAnalysis/SentencesParserTask.cs
+++ TextAnalysis/SentencesParserTask.cs
@@ -2,22 +2,20 @@
 
 static class SentencesParserTask
 {
-    public static readonly char[] SentencesSeparators = ['.', '!', '?', ';', ':', '(', ')'];
+    private static readonly char[] SentencesSeparators = ['.', '!', '?', ';', ':', '(', ')'];
 
     public static List<List<string>> ParseSentences(string text)
     {
-        var sentencesList = text
-                            .Split(SentencesSeparators, StringSplitOptions.RemoveEmptyEntries);
-
-        var wordsSeparators = sentencesList
-                              .SelectMany(s => s.Where(symbol => !char.IsLetter(symbol) && symbol != '\''))
+        var wordsSeparators = text
+                              .Where(symbol => !char.IsLetter(symbol) && symbol != '\'')
                               .ToHashSet()
                               .ToArray();
-        var parsedSentences = sentencesList
+        var parsedSentences = text
+                              .ToLower()
+                              .Split(SentencesSeparators, StringSplitOptions.RemoveEmptyEntries)
                               .Select(sentence => sentence
-                                           .ToLower()
-                                           .Split(wordsSeparators, StringSplitOptions.RemoveEmptyEntries)
-                                           .ToList())
+                                                  .Split(wordsSeparators, StringSplitOptions.RemoveEmptyEntries)
+                                                  .ToList())
                               .Where(sentence => sentence.Count > 0)
                               .ToList();
         return parsedSentences;
