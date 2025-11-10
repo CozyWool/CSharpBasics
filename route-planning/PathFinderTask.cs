1565514ec134f0e2f1a168cc10159c42d54ff7bd
CozyWool <vsergeev201530@gmail.com>
2025-11-10 11:31:27 +0500

    route-planning 50/50 result

--- route-planning/PathFinderTask.cs
+++ route-planning/PathFinderTask.cs
@@ -1,20 +1,67 @@
+using System;
 using System.Drawing;
 
 namespace RoutePlanning;
 
 public static class PathFinderTask
 {
-	public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
-	{
-		var bestOrder = MakeTrivialPermutation(checkpoints);
-		return bestOrder;
-	}
+    private static double minDistance = double.MaxValue;
+    private static int[] best;
 
-	private static int[] MakeTrivialPermutation(Point[] checkpoints)
-	{
-		var bestOrder = new int[checkpoints];
-		for (var i = 0; i < bestOrder.Length; i++)
-			bestOrder[i] = i;
-		return bestOrder;
-	}
+    public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
+    {
+        minDistance = double.MaxValue;
+        var bestOrder = MakeTrivialPermutation(checkpoints);
+        return bestOrder;
+    }
+
+    private static int[] MakeTrivialPermutation(Point[] checkpoints)
+    {
+        var bestOrder = new int[checkpoints.Length];
+        for (var i = 0; i < bestOrder.Length; i++)
+            bestOrder[i] = i;
+        MakePermutations(checkpoints, bestOrder);
+        return best;
+    }
+
+    static void MakePermutations(Point[] checkpoints, int[] permutation, int position = 0, double distance = 0)
+    {
+        if (position == permutation.Length)
+        {
+            var currentDistance = checkpoints.GetPathLength(permutation);
+            if (currentDistance < minDistance)
+            {
+                minDistance = currentDistance;
+                best = new int[permutation.Length];
+                for (var i = 0; i < permutation.Length; i++)
+                {
+                    best[i] = permutation[i];
+                }
+            }
+            return;
+        }
+
+        for (var i = 0; i < permutation.Length; i++)
+        {
+            var index = Array.IndexOf(permutation, i, 0, position);
+            if (index != -1)
+            {
+                continue;
+            }
+
+            permutation[position] = i;
+            if (position > 0)
+            {
+                var previous = checkpoints[permutation[position - 1]];
+                var current = checkpoints[permutation[position]];
+                distance += previous.DistanceTo(current);
+            }
+
+            if (distance >= minDistance)
+            {
+                break;
+            }
+            MakePermutations(checkpoints, permutation, position + 1);
+        }
+    }
 }
