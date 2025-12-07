55391d0d30f70484ba3a1d249524b1bf76306d5c
CozyWool <vsergeev201530@gmail.com>
2025-12-07 20:38:01 +0500

    HotelAccounting done

---
+++ HotelAccounting/AccountingModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace HotelAccounting;
+
+public class AccountingModel : ModelBase
+{
+    private double _price;
+    private int _nightsCount;
+    private double _discount;
+    private double _total;
+
+    public double Price
+    {
+        get => _price;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            _price = value;
+            UpdateTotal();
+            Notify(nameof(Price));
+        }
+    }
+
+
+    public int NightsCount
+    {
+        get => _nightsCount;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            _nightsCount = value;
+            Notify(nameof(NightsCount));
+            UpdateTotal();
+        }
+    }
+
+    public double Discount
+    {
+        get => _discount;
+        set
+        {
+            if (value > 100.0)
+            {
+                throw new ArgumentException();
+            }
+
+            _discount = value;
+            Notify(nameof(Discount));
+            UpdateTotal();
+        }
+    }
+
+    private void UpdateTotal()
+    {
+        _total = _price * _nightsCount * (1 - _discount / 100);
+        Notify(nameof(Total));
+    }
+
+    public double Total
+    {
+        get => _total;
+        set => Discount = 100 * (1 - value / (_price * _nightsCount));
+    }
+}
