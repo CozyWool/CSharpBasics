db1e3c1d0f43878285aae90a2171673ceb603433
CozyWool <vsergeev201530@gmail.com>
2025-12-07 17:50:40 +0500

    ReadOnlyVector done

---
+++ ReadonlyVector/ReadonlyVector/ReadOnlyVector.cs
@@ -0,0 +1,22 @@
+﻿namespace ReadOnlyVector;
+
+public class ReadOnlyVector(double x, double y)
+{
+    public readonly double X = x;
+    public readonly double Y = y;
+
+    public ReadOnlyVector WithX(double x)
+    {
+        return new ReadOnlyVector(x, Y);
+    }
+
+    public ReadOnlyVector WithY(double y)
+    {
+        return new ReadOnlyVector(X, y);
+    }
+
+    public ReadOnlyVector Add(ReadOnlyVector other)
+    {
+        return new ReadOnlyVector(X + other.X, Y + other.Y);
+    }
+}
