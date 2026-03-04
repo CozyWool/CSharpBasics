9c3a93258401de1b4bb97ebdcf5ab4c5545bf03f
CozyWool <vsergeev201530@gmail.com>
2026-03-04 19:28:52 +0500

    Brainfuck LoopCommands done

--- brainfuck/BrainfuckLoopCommands.cs
+++ brainfuck/BrainfuckLoopCommands.cs
@@ -1,11 +1,56 @@
+using System;
+using System.Collections.Generic;
+
 namespace func.brainfuck
 {
-	public class BrainfuckLoopCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm)
-		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
-		}
-	}
+    public class BrainfuckLoopCommands
+    {
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            var loopSymbols = FindMatchingBrackets(vm);
+
+            vm.RegisterCommand('[', StartLoop(loopSymbols));
+            vm.RegisterCommand(']', EndLoop(loopSymbols));
+        }
+
+        private static int[] FindMatchingBrackets(IVirtualMachine vm)
+        {
+            var loopSymbols = new int[vm.Instructions.Length];
+            var bracketStack = new Stack<int>();
+            for (var i = 0; i < vm.Instructions.Length; i++)
+            {
+                if (vm.Instructions[i] == '[')
+                {
+                    bracketStack.Push(i);
+                }
+
+                if (vm.Instructions[i] == ']')
+                {
+                    var openBracket = bracketStack.Pop();
+                    loopSymbols[openBracket] = i;
+                    loopSymbols[i] = openBracket;
+                }
+            }
+
+            return loopSymbols;
+        }
+
+        private static Action<IVirtualMachine> StartLoop(int[] loopSymbols) =>
+            b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 0)
+                {
+                    b.InstructionPointer = loopSymbols[b.InstructionPointer];
+                }
+            };
+
+        private static Action<IVirtualMachine> EndLoop(int[] loopSymbols) =>
+            b =>
+            {
+                if (b.Memory[b.MemoryPointer] != 0)
+                {
+                    b.InstructionPointer = loopSymbols[b.InstructionPointer];
+                }
+            };
+    }
 }
