2a732030076921bb2597f2ea07822ac61b41c2ab
CozyWool <vsergeev201530@gmail.com>
2026-03-14 15:11:04 +0500

    ControlTask done

--- rocket/ControlTask.cs
+++ rocket/ControlTask.cs
@@ -1,9 +1,19 @@
+using System;
+
 namespace func_rocket;
 
 public class ControlTask
 {
-	public static Turn ControlRocket(Rocket rocket, Vector target)
-	{
-		return Turn.None;
-	}
+    public static Turn ControlRocket(Rocket rocket, Vector target)
+    {
+        var toTarget = target - rocket.Location;
+        var angleDiff = toTarget.Angle - rocket.Direction;
+
+        if (Math.Abs(angleDiff) < 0.8 || Math.Abs(toTarget.Angle - rocket.Velocity.Angle) < 0.8)
+        {
+            angleDiff = -(rocket.Velocity.Angle + rocket.Direction) / 2 + toTarget.Angle;
+        }
+
+        return angleDiff > 0 ? Turn.Right : (angleDiff < 0 ? Turn.Left : Turn.None);
+    }
 }
