ba01f7cbbc67a7c0819a5f25c9ab42a23d5c2bda
CozyWool <vsergeev201530@gmail.com>
2026-03-18 11:45:15 +0500

    LevelsTask fixed

--- rocket/LevelsTask.cs
+++ rocket/LevelsTask.cs
@@ -5,50 +5,56 @@ namespace func_rocket;
 
 public class LevelsTask
 {
-    static readonly Physics standardPhysics = new();
+    private static readonly Physics _standardPhysics = new();
+    private static readonly Rocket _rocket = new(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI);
+    private static readonly Vector _target = new(700, 500);
+    private const int WhiteHoleCoefficient = 140;
+    private const int BlackHoleCoefficient = 300;
 
     public static IEnumerable<Level> CreateLevels()
     {
-        var rocket = new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI);
-        var target = new Vector(700, 500);
+        var anomaly = (_rocket.Location + _target) / 2;
 
-        var anomaly = (rocket.Location + target) / 2;
+        var whiteHoleGravity = GetHoleGravity(WhiteHoleCoefficient, false);
+        var blackHoleGravity = GetHoleGravity(BlackHoleCoefficient, true, anomaly);
 
-        yield return CreateLevel("Zero", rocket, target, (_, _) => Vector.Zero);
+        yield return CreateLevel("Zero", (_, _) => Vector.Zero);
 
-        yield return CreateLevel("Heavy", rocket, target, (_, _) => new Vector(0, 0.9));
-        yield return CreateLevel("Up", rocket, target, (size, v) => GetUpGravity(v, size));
-        yield return CreateLevel("WhiteHole", rocket, target, (_, v) => GetWhiteHole(target, v));
-        yield return CreateLevel("BlackHole", rocket, target, (_, v) => GetBlackHole(anomaly, v));
-        yield return CreateLevel("BlackAndWhite", rocket, target,
-                                 (_, v) => (GetBlackHole(anomaly, v) + GetWhiteHole(target, v)) / 2);
+        yield return CreateLevel("Heavy", (_, _) => new Vector(0, 0.9));
+        yield return CreateLevel("Up", GetUpGravity());
+        yield return CreateLevel("WhiteHole", whiteHoleGravity);
+        yield return CreateLevel("BlackHole", blackHoleGravity);
+        yield return CreateLevel("BlackAndWhite",
+                                 (size, v) => (whiteHoleGravity(size, v) + blackHoleGravity(size, v)) / 2);
     }
 
-    private static Level CreateLevel(string name, Rocket rocket, Vector target, Gravity gravity) =>
-        new(name, rocket, target, gravity, standardPhysics);
+    private static Level CreateLevel(string name, Gravity gravity, Rocket? rocket = null, Vector? target = null) =>
+        new(name, rocket ?? _rocket, target ?? _target, gravity, _standardPhysics);
 
-    private static Vector GetUpGravity(Vector v, Vector size)
+    private static Gravity GetUpGravity()
     {
-        var lowerBorder = new Vector(v.X, size.Y);
-        var d = (lowerBorder - v).Length;
-        var module = -(300 / (d + 300.0));
-        return new Vector(0, module);
+        return (size, v) =>
+               {
+                   var lowerBorder = new Vector(v.X, size.Y);
+                   var d = (lowerBorder - v).Length;
+                   var module = -(300 / (d + 300.0));
+                   return new Vector(0, module);
+               };
     }
 
-
-    private static Vector GetBlackHole(Vector anomaly, Vector v)
+    private static Gravity GetHoleGravity(int coefficient, bool isAttractive, Vector? target = null)
     {
-        var direction = anomaly - v;
-        var d = direction.Length;
-        var module = 300 * d / (d * d + 1);
-        return direction.Normalize() * module;
-    }
+        return (_, v) =>
+               {
+                   var direction = v - (target ?? _target);
+                   if (isAttractive)
+                   {
+                       direction *= -1;
+                   }
 
-    private static Vector GetWhiteHole(Vector target, Vector v)
-    {
-        var direction = v - target;
-        var d = direction.Length;
-        var module = 140 * d / (d * d + 1);
-        return direction.Normalize() * module;
+                   var d = direction.Length;
+                   var module = coefficient * d / (d * d + 1);
+                   return direction.Normalize() * module;
+               };
     }
 }
