cd7f810758e36aa810ce940fb9e58692e88acbff
CozyWool <vsergeev201530@gmail.com>
2026-03-18 14:04:22 +0500

    StatisticsTask done

--- linq-slideviews/StatisticsTask.cs
+++ linq-slideviews/StatisticsTask.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace linq_slideviews;
 
 public class StatisticsTask
 {
-	public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
-	{
-		throw new NotImplementedException();
-	}
+    private const double MinDeltaMinutes = 1;
+    private const double MaxDeltaMinutes = 120;
+
+    public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
+    {
+        return visits
+               .GroupBy(visit => visit.UserId)
+               .SelectMany(group => group
+                                .OrderBy(visit => visit.DateTime)
+                                .Bigrams()
+                                .Where(bigram => bigram.First.SlideType == slideType)
+                                .Select(bigram => (bigram.Second.DateTime - bigram.First.DateTime).TotalMinutes)
+                                .Where(delta => delta is >= MinDeltaMinutes and <= MaxDeltaMinutes))
+               .DefaultIfEmpty(0.0)
+               .Median();
+    }
 }
