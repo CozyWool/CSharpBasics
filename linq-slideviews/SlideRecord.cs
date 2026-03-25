3bdb498921ad3d710d76c2216ebee79ea2524a45
CozyWool <vsergeev201530@gmail.com>
2026-03-25 09:23:14 +0500

    StatisticsTask fixed

--- linq-slideviews/SlideRecord.cs
+++ linq-slideviews/SlideRecord.cs
@@ -25,9 +25,21 @@ public class SlideRecord
 
 	public override bool Equals(object obj)
 	{
-		if (ReferenceEquals(null, obj)) return false;
-		if (ReferenceEquals(this, obj)) return true;
-		if (obj.GetType() != this.GetType()) return false;
+		if (ReferenceEquals(null, obj))
+		{
+			return false;
+		}
+
+		if (ReferenceEquals(this, obj))
+		{
+			return true;
+		}
+
+		if (obj.GetType() != this.GetType())
+		{
+			return false;
+		}
+
 		return Equals((SlideRecord) obj);
 	}
 
--- linq-slideviews/StatisticsTask.cs
+++ linq-slideviews/StatisticsTask.cs
@@ -1,23 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace linq_slideviews;
 
 public class StatisticsTask
 {
-    private const double MinDeltaMinutes = 1;
-    private const double MaxDeltaMinutes = 120;
-
     public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
     {
         return visits
                .GroupBy(visit => visit.UserId)
                .SelectMany(group => group
-                                .OrderBy(visit => visit.DateTime)
-                                .Bigrams()
-                                .Where(bigram => bigram.First.SlideType == slideType)
-                                .Select(bigram => (bigram.Second.DateTime - bigram.First.DateTime).TotalMinutes)
-                                .Where(delta => delta is >= MinDeltaMinutes and <= MaxDeltaMinutes))
+                                    .OrderBy(visit => visit.DateTime)
+                                    .Bigrams()
+                                    .Where(bigram => bigram.First.SlideType == slideType)
+                                    .Select(bigram => (bigram.Second.DateTime - bigram.First.DateTime).TotalMinutes)
+                                    .Where(delta => TimeSpan.FromMinutes(1).TotalMinutes <= delta &&
+                                                    delta <= TimeSpan.FromHours(2).TotalMinutes))
                .DefaultIfEmpty(0.0)
                .Median();
     }
