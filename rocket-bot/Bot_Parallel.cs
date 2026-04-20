6fd841197035a690d69f1c4da989f4860d7087de
CozyWool <vsergeev201530@gmail.com>
2026-04-20 08:06:46 +0500

    Bot_Parallel done

--- rocket-bot/Bot_Parallel.cs
+++ rocket-bot/Bot_Parallel.cs
@@ -7,19 +7,31 @@ namespace rocket_bot;
 
 public partial class Bot
 {
-	public Rocket GetNextMove(Rocket rocket)
-	{
-		// TODO: распараллелить запуск SearchBestMove
-		var (turn, score) = CreateTasks(rocket)
-			.First()
-			.GetAwaiter()
-			.GetResult();
-		
-		return rocket.Move(turn, level);
-	}
-	
-	public List<Task<(Turn Turn, double Score)>> CreateTasks(Rocket rocket)
-	{
-		return [Task.Run(() => SearchBestMove(rocket, new Random(random.Next()), iterationsCount))];
-	}
+    public Rocket GetNextMove(Rocket rocket)
+    {
+        var bestResult = GetBestMoveAsync(rocket).Result;
+
+        return rocket.Move(bestResult.Turn, level);
+    }
+
+    private async Task<(Turn Turn, double Score)> GetBestMoveAsync(Rocket rocket)
+    {
+        var tasks = CreateTasks(rocket);
+        var results = await Task.WhenAll(tasks);
+
+        return results.MaxBy(x => x.Score);
+    }
+
+    public List<Task<(Turn Turn, double Score)>> CreateTasks(Rocket rocket)
+    {
+        var tasks = new List<Task<(Turn Turn, double Score)>>();
+
+        for (var i = 0; i < threadsCount; i++)
+        {
+            var seed = random.Next();
+            tasks.Add(Task.Run(() => SearchBestMove(rocket, new Random(seed), iterationsCount / threadsCount)));
+        }
+
+        return tasks;
+    }
 }
