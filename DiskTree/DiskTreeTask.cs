64a0e2edcf8156f186cf9461dd3aab0983fe0209
CozyWool <vsergeev201530@gmail.com>
2026-05-03 14:02:11 +0500

    DiskTreeTask done

--- DiskTree/DiskTreeTask.cs
+++ DiskTree/DiskTreeTask.cs
@@ -1,12 +1,43 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 
 namespace DiskTree;
 
-public class DiskTreeTask
+public static class DiskTreeTask
 {
-    public static IEnumerable Solve(List<string> input)
+    private class DiskTreeNode
     {
-        throw new System.NotImplementedException();
+        public SortedList<string, DiskTreeNode> Children { get; } = new(StringComparer.Ordinal);
+    }
+
+    public static List<string> Solve(List<string> input)
+    {
+        var root = new DiskTreeNode();
+
+        foreach (var path in input)
+        {
+            var directories = path.Split('\\', StringSplitOptions.RemoveEmptyEntries);
+            var current = root;
+
+            foreach (var directory in directories)
+            {
+                current.Children.TryAdd(directory, new DiskTreeNode());
+                current = current.Children[directory];
+            }
+        }
+
+        var result = new List<string>();
+        result.FillResult(root);
+        return result;
+    }
+
+    private static void FillResult(this List<string> result, DiskTreeNode node, int depth = -1)
+    {
+        foreach (var (directory, nextNode) in node.Children)
+        {
+            var indent = new string(' ', depth + 1);
+            result.Add($"{indent}{directory}");
+            result.FillResult(nextNode, depth + 1);
+        }
     }
 }
